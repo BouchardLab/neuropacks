69b1f5fc3b658cf59ed6bb10279af8638bf75439
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2018-12-31 17:56:44 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='neuropacks',
-    version='0.1.3',
+    version='0.1',
     # What does your project relate to?
-    keywords='UoI',
+    keywords='neuroscience',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(),
 
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'matplotlib', 'scikit-learn'],
+    install_requires=[
+        'h5py'
+        'matplotlib',
+        'numpy',
+        'pyuoi',
+        'scikit-learn',
+        'scipy'
+    ]
 )
