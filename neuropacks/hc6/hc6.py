7ea6ba61769a2b6b221977442cfec226a396a095
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2019-03-28 10:58:57 -0700

    HC6 data acquisition complete

--- neuropacks/hc6/hc6.py
+++ neuropacks/hc6/hc6.py
@@ -11,6 +11,7 @@ class HC6():
         self.__initialize_class()
 
     def __initialize_class(self):
+        """Initializes the datasets in the provided directory."""
         # first look at cell info
         cell_info_path = os.path.join(self.directory,
                                       self.base + 'cellinfo.mat')
@@ -27,46 +28,76 @@ class HC6():
         for day in self.valid_days:
             self.n_tetrodes[day] = cell_info[0, day][0, 0].size
 
-        # number of units
-        self.n_units_per_tetrode = {}
+        # data dictionaries
         self.n_units = {}
+        self.n_units_per_tetrode = {}
+        self.positions = {}
+        self.spike_times = {}
+
+        # iterate over the days
         for day in range(self.n_days):
-            self.n_units_per_tetrode[day] = {}
+            # initialize top level entry of dictionaries
             self.n_units[day] = {}
+            self.n_units_per_tetrode[day] = {}
+            self.positions[day] = {}
+            self.spike_times[day] = {}
+
+            if day in self.valid_days:
+                # obtain data for position
+                position_path = os.path.join(
+                    self.directory,
+                    self.base + 'pos%02d' % (day + 1) + '.mat'
+                )
+                positions = loadmat(
+                    position_path,
+                    struct_as_record=False
+                )['pos'][0, day]
+
+                # obtain data for spikes
+                spikes_path = os.path.join(
+                    self.directory,
+                    self.base + 'spikes%02d' % (day + 1) + '.mat'
+                )
+                spikes = loadmat(
+                    spikes_path,
+                    struct_as_record=False
+                )['spikes'][0, day]
+
+            # iterate over epochs in day
             for epoch in range(self.n_epochs[day]):
-                self.n_units_per_tetrode[day][epoch] = {}
                 self.n_units[day][epoch] = 0
+                self.n_units_per_tetrode[day][epoch] = {}
+                self.spike_times[day][epoch] = {}
+
+                # grab positions
+                xy = positions[0, epoch][0, 0].data
+                positions_epoch = {}
+                positions_epoch['time'] = xy[:, 0]
+                positions_epoch['x'] = xy[:, 1]
+                positions_epoch['y'] = xy[:, 2]
+                self.positions[day][epoch] = positions_epoch
+
+                # we will be tetrode agnostic when we store the spike times of
+                # the units
+                unit_idx = 0
+                # iterate over tetrodes
                 for tetrode in range(self.n_tetrodes[day]):
-                    n_units = cell_info[0, day][0, epoch][0, tetrode].size
+                    n_units = spikes[0, epoch][0, tetrode].size
                     self.n_units_per_tetrode[day][epoch][tetrode] = n_units
                     self.n_units[day][epoch] += n_units
 
-        # get positions
-        self.positions = {}
-        for day in self.valid_days:
-            self.positions[day] = {}
-            position_path = os.path.join(self.directory,
-                                         self.base + 'pos%02d' % (day + 1)
-                                         + '.mat')
-            positions = loadmat(position_path, struct_as_record=False)
-            for epoch in range(self.n_epochs[day]):
-                data = positions['pos'][0, day][0, epoch][0, 0].data
-                self.positions[day][epoch] = {}
-                self.positions[day][epoch]['time'] = data[:, 0]
-                self.positions[day][epoch]['x'] = data[:, 1]
-                self.positions[day][epoch]['y'] = data[:, 2]
-
-        # get spike times
-        self.spike_times = {}
-        for day in self.valid_days:
-            self.spike_times[day] = {}
-            spikes_path = os.path.join(self.directory,
-                                       self.base + 'spikes%02d' % (day + 1)
-                                       + '.mat')
-            spike_times = loadmat(spikes_path, struct_as_record=False)
-            for epoch in range(self.n_epochs[day]):
-                self.spike_times[day][epoch] = {}
-                for tetrode in range(self.n_tetrodes[day]):
-                    for unit in range(self.n_units_per_tetrode[day][epoch][tetrode]):
-                        self.spike_times[day][epoch][unit]
-
+                    # iterate over units in the tetrode
+                    for unit in range(n_units):
+                        unit_data = spikes[0, epoch][0, tetrode][0, unit]
+                        if unit_data.size != 0:
+                            spike_times = unit_data[0, 0].data
+                            if spike_times.size > 0:
+                                self.spike_times[day][epoch][unit_idx] = \
+                                    spike_times[:, 0]
+                            else:
+                                self.spike_times[day][epoch][unit_idx] = \
+                                    np.array([])
+                        else:
+                            self.spike_times[day][epoch][unit_idx] = \
+                                np.array([])
+                        unit_idx += 1
