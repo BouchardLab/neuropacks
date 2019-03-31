75fe4553264dbba0f370d9dd50599b38c0d69124
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2019-03-30 21:29:57 -0700

    HC6 tuning

--- neuropacks/hc6/hc6.py
+++ neuropacks/hc6/hc6.py
@@ -6,6 +6,34 @@ from scipy.io import loadmat
 
 class HC6():
     def __init__(self, directory):
+        """Processes and provides response matrices for the hc6 hippocampus
+        dataset obtained from the Frank lab.
+
+        Parameters
+        ----------
+        directory : string
+            The path to the top level folder containing the data.
+
+        Attributes
+        ----------
+        base : string
+
+        n_days : int
+
+        valid_days : ndarray
+
+        valid_days_labels : ndarray
+
+        n_tetrodes : ndarray
+
+        n_units : nested dict
+
+        n_units_per_tetrode : nested dict
+
+        positions : nested dict
+
+        spike_times : nested dict
+        """
         self.directory = directory
         self.base = os.path.basename(os.path.normpath(self.directory)).lower()
         self.__initialize_class()
@@ -102,19 +130,230 @@ class HC6():
                                 np.array([])
                         unit_idx += 1
 
-    def get_tuning_matrix(self, day, epoch, n_gaussians):
+    def get_positions(self, day, epoch, return_time=False):
+        """Obtain the mouse position by time for a given experiment.
+
+        Parameters
+        ----------
+        day : int
+            The index of the day.
+
+        epoch : int
+            The index of the epoch.
+
+        return_time : bool
+            If True, the timestamps will be returned.
+
+        Returns
+        -------
+        xs : ndarray
+            The x positions.
+
+        ys : ndarray
+            The y positions.
+
+        times : ndarray
+            The timestamps for each position; optional.
+        """
+        if day not in self.valid_days:
+            raise ValueError('Day does not have any recordings.')
+
         positions = self.positions[day][epoch]
+        times = positions['time']
         xs = positions['x']
         ys = positions['y']
 
+        if return_time:
+            return xs, ys, times
+        else:
+            return xs, ys
+
+    def get_spike_times(self, day, epoch, unit):
+        """Obtain the mouse position by time for a given experiment.
+
+        Parameters
+        ----------
+        day : int
+            The index of the day.
+
+        epoch : int
+            The index of the epoch.
+
+        unit : int
+            The index of the unit.
+
+        Returns
+        -------
+        spike_times : ndarray
+            The timestamps for each spike; optional.
+        """
+        if day not in self.valid_days:
+            raise ValueError('Day does not have any recordings.')
+
+        max_unit_idx = self.n_units[day][epoch]
+        if (unit >= max_unit_idx) or (unit < 0):
+            raise ValueError('Unit index does not exist.')
+
+        spike_times = self.spike_times[day][epoch][unit]
+
+        return spike_times
+
+    def get_binned_positions(self, day, epoch, bin_width=0.5):
+        """Bin the mouse positions according to a bin width.
+
+        Parameters
+        ----------
+        day : int
+            The index of the day.
+
+        epoch : int
+            The index of the epoch.
+
+        bin_width : float
+            The width of the bin, in seconds.
+
+        Returns
+        -------
+        x_binned : ndarray
+            The mean x position in each bin.
+
+        y_binned : ndarray
+            The mean y position in each bin.
+        """
+        xs, ys, times = self.get_positions(day, epoch, return_time=True)
+
+        # calculate number of bins
+        n_bins = int(np.ceil(
+            (times[-1] - times[0])/bin_width
+        ))
+        bins = np.arange(n_bins + 1) * bin_width + times[0]
+        bin_indices = np.digitize(times, bins=bins) - 1
+
+        x_binned = np.zeros(n_bins)
+        y_binned = np.zeros(n_bins)
+
+        for bin_idx in range(n_bins):
+            indices = np.argwhere(bin_indices == bin_idx).ravel()
+            x_binned[bin_idx] = np.mean(xs[indices])
+            y_binned[bin_idx] = np.mean(ys[indices])
+
+        return x_binned, y_binned
+
+    def get_design_matrix(self, day, epoch, bin_width=0.5, n_gaussians=5):
+        """Parameterize the positions using gaussian basis functions.
+
+        Parameters
+        ----------
+        day : int
+            The index of the day.
+
+        epoch : int
+            The index of the epoch.
+
+        bin_width : float
+            The width of the bin, in seconds.
+
+        n_gaussians : int
+            The number of gaussians to use in one dimension (the total number
+            of basis functions will be n_gaussians**2).
+
+        Returns
+        -------
+        X : ndarray, shape (n_samples, n_gaussians**2)
+            The design matrix.
+        """
+        xs, ys, times = self.get_positions(day, epoch, return_time=True)
+
+        # gaussian basis functions
         x_std = np.std(xs)
         y_std = np.std(ys)
-        x_means = x_std * np.linspace(-2, 2, n_gaussians) + xs.mean()
-        y_means = y_std * np.linspace(-2, 2, n_gaussian) + ys.mean()
 
-        tuning_std_x = x_std / 2
-        tuning_std_y = y_std / 2
+        # calculate means of gaussian bfs
+        x_bf_means = x_std * np.linspace(-2, 2, n_gaussians) + xs.mean()
+        y_bf_means = y_std * np.linspace(-2, 2, n_gaussians) + ys.mean()
+        means = np.transpose([
+            np.tile(x_bf_means, n_gaussians),
+            np.repeat(y_bf_means, n_gaussians)
+        ])
 
-        X = np.zeros(())
-        for idx, (x, y) in enumerate(zip(xs, ys)):
+        # calculate stds of gaussian bfs
+        x_bf_std = x_std / 2
+        y_bf_std = y_std / 2
+        covar = np.array([[x_bf_std**2, 0], [0, y_bf_std**2]])
+        inv_covar = np.linalg.inv(covar)
+        norm = 1. / np.sqrt((2 * np.pi)**2 * np.linalg.det(covar))
 
+        x_binned, y_binned = self.get_binned_positions(
+            day=day,
+            epoch=epoch,
+            bin_width=bin_width
+        )
+        xy_binned = np.transpose([x_binned, y_binned])
+        n_samples = x_binned.size
+
+        X = np.zeros((n_samples, n_gaussians**2))
+
+        for sample_idx, xy in enumerate(xy_binned):
+            for gaussian_idx, mean in enumerate(means):
+                arg = xy - mean
+                X[sample_idx, gaussian_idx] = norm * np.exp(
+                    -0.5 * np.dot(arg, np.dot(inv_covar, arg))
+                )
+
+        return X, (means, covar)
+
+    def get_response_matrix(
+        self, day, epoch, bin_width, transform='square_root', clean=False
+    ):
+        """Create the response matrix by binning the spike times.
+
+        Parameters
+        ----------
+        bin_width : float
+            The width of the bins, in seconds.
+
+        transform : string
+            The transform to apply to the spike counts. Available option is
+            'square_root'. If None, no transform is applied.
+
+        Returns
+        -------
+        Y : nd-array, shape (n_trials, n_neurons)
+            Response matrix containing (transformed) spike counts of each
+            neuron.
+        """
+        # grab times
+        _, _, times = self.get_positions(day, epoch, return_time=True)
+
+        # number of units
+        spike_times = self.spike_times[day][epoch]
+        n_units = len(spike_times)
+
+        # calculate number of bins
+        n_bins = int(np.ceil(
+            (times[-1] - times[0])/bin_width
+        ))
+        bins = np.arange(n_bins + 1) * bin_width + times[0]
+
+        Y = np.zeros((n_bins, n_units))
+        silent_neurons = np.array([])
+        # iterate over spike time arrays in corresponding dictionary
+        for idx, (key, spikes) in enumerate(spike_times.items()):
+            # bin the spike times
+            binned_spike_counts = np.histogram(spikes, bins=bins)[0]
+
+            # apply a transform
+            if transform is None:
+                Y[:, idx] = binned_spike_counts
+            elif transform == 'square_root':
+                Y[:, idx] = np.sqrt(binned_spike_counts)
+            else:
+                raise ValueError('Transform %s is not valid.' % transform)
+
+            if spikes.size == 0:
+                silent_neurons = np.append(silent_neurons, idx)
+
+        if clean:
+            Y = np.delete(Y, silent_neurons, axis=1)
+
+        return Y
