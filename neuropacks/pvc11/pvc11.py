fdeb632d2f009eac7851d7a70bac520d9cc55d08
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2021-04-05 09:57:02 -0700

    bring pvc11 up to style; update modulations and preferences

--- neuropacks/pvc11/pvc11.py
+++ neuropacks/pvc11/pvc11.py
@@ -16,20 +16,15 @@ class PVC11():
         ----------
         data_path : string
             The path to the PVC11 dataset.
-
         events : nd-array, shape (n_neurons, n_stimuli, n_trials)
             A numpy array containing the spiking responses for each neuron
             to each stimulus on each trial.
-
         n_neurons : int
             The number of neurons in the experiment.
-
         n_stimuli : int
             The number of stimuli shown in the experiment.
-
         n_trials : int
             The number of trials for each stimulus in the experiment.
-
         spike_counts : nd-array, shape (n_neurons, n_stimuli, n_trials)
             A numpy array containing the spiking count for each neuron
             to each stimulus on each trial.
@@ -40,12 +35,11 @@ class PVC11():
 
     def load_data(self):
         """Loads the PVC-11 data."""
-
-        # load data
+        # Load data
         data = loadmat(self.data_path, struct_as_record=False)
-        # extract events
+        # Extract events
         self.events = data['data'][0, 0].EVENTS
-        # extract experiment parameters
+        # Extract experiment parameters
         self.n_neurons, self.n_stimuli, self.n_trials = self.events.shape
 
     def get_spike_counts(self):
@@ -56,16 +50,14 @@ class PVC11():
         spike_counts : nd-array, shape (n_neurons, n_stim, n_trials)
             An array containing spike counts over all neurons and trials.
         """
-
         self.spike_counts = np.zeros(
             (self.n_neurons, self.n_stimuli, self.n_trials)
         )
-
-        # iterate over all trials, stimuli, and neurons
+        # Iterate over all trials, stimuli, and neurons
         for neuron in range(self.n_neurons):
             for stim in range(self.n_stimuli):
                 for trial in range(self.n_trials):
-                    # compute all spike counts in the trial
+                    # Compute all spike counts in the trial
                     self.spike_counts[neuron, stim, trial] = \
                         self.events[neuron, stim, trial].size
 
@@ -90,7 +82,6 @@ class PVC11():
         ----------
         form : string
             The structure of the design matrix.
-
         angles : ndarray
             The angles to obtain a design matrix for. If None, all angles will
             be used.
@@ -107,29 +98,24 @@ class PVC11():
             unique_angles = np.unique(angles)
 
         if form == 'angle':
-            # the angles for each trial; no extra dimension required
+            # The angles for each trial; no extra dimension required
             X = angles
-
         elif form == 'label':
             X = (angles/30).astype('int64')
-
         elif form == 'cosine':
             X = np.zeros((angles.size, 2))
             X[:, 0] = np.cos(np.deg2rad(angles))
             X[:, 1] = np.sin(np.deg2rad(angles))
-
         elif form == 'cosine2':
             X = np.zeros((angles.size, 2))
             X[:, 0] = np.cos(2 * np.deg2rad(angles))
             X[:, 1] = np.sin(2 * np.deg2rad(angles))
-
         elif form == 'one_hot':
             X = np.zeros((angles.size, unique_angles.size))
 
             for idx, angle in enumerate(angles):
                 angle_idx = np.asscalar(np.argwhere(unique_angles == angle))
                 X[idx, angle_idx] = 1
-
         elif form == 'gbf':
             n_bf = kwargs.get('n_bf', 20)
             lower_bound = kwargs.get('lower_bound', 15)
@@ -145,7 +131,6 @@ class PVC11():
                 X[idx] = norm * np.exp(
                     -(angle - means)**2 / (2 * var)
                 )
-
         elif form == 'cbf':
             n_bf = kwargs.get('n_bf', 30)
             lower_bound = kwargs.get('lower_bound', 0)
@@ -157,7 +142,6 @@ class PVC11():
 
             for idx, angle in enumerate(angles):
                 X[idx] = np.cos(2 * np.deg2rad(angle - means))
-
         else:
             raise ValueError("Incorrect design matrix form specified.")
 
@@ -188,7 +172,7 @@ class PVC11():
             (self.n_neurons, self.n_stimuli * self.n_trials)
         ).T
 
-        # apply any desired transform
+        # Apply desired transform
         if transform == 'square_root':
             Y = np.sqrt(Y)
         elif transform == 'log':
@@ -209,13 +193,10 @@ class PVC11():
         form : string
             The type of design matrix used. Valid options are "angle",
             "cosine", "cosine_speed", "one_hot", and "speed_hot".
-
         tuning_coefs : nd-array, shape (n_features)
             The coefficients describing the tuning of the neuron.
-
         intercept : float, optional
             The intercept.
-
         angles : ndarray
             The angles over which to calculate the tuning curve. If None, a
             default set is used.
@@ -224,25 +205,24 @@ class PVC11():
         -------
         tuning_curve : ndarray
             The responses across the set of angles.
-
         """
-        # if no angles are provided, calculate a set
+        # If no angles are provided, calculate a set
         if angles is None:
             angles = self.get_angles()
             if form != 'one_hot':
                 angles = np.linspace(angles[0], angles[-1], 1000)
 
-        # calculate design matrix for angles
+        # Calculate design matrix for angles
         X = self.get_design_matrix(form=form, angles=angles)
 
-        # calculate tuning curve
+        # Calculate tuning curve
         tuning_curve = np.dot(X, tuning_coefs)
         if intercept is not None:
             tuning_curve += intercept
 
         return angles, tuning_curve
 
-    def get_tuning_modulation_and_preference(self, form, tuning_coefs):
+    def get_tuning_modulation_and_preference(self, tuning_coefs, form='cosine2'):
         """Extracts the tuning modulation and preference from a set
         of tuning coefficients.
 
@@ -251,7 +231,6 @@ class PVC11():
         form : string
             The type of design matrix used. Valid options are "angle",
             "cosine", "cosine_speed", "one_hot", and "speed_hot".
-
         tuning_coefs : nd-array, shape (n_neurons, n_features)
             The coefficients describing the tuning of each neuron.
 
@@ -259,38 +238,34 @@ class PVC11():
         -------
         modulations : nd-array of floats
             The modulation (min-to-max distance) for each neuron.
-
         preferences : nd-array of floats
             The preference (location of tuning maximum) for each neuron.
         """
-
         if form == 'cosine':
-            # y = b0 + c1 cos(x) + c2 sin(x)
+            # Splt up cosine coefficients
             c1 = tuning_coefs[..., 0]
             c2 = tuning_coefs[..., 1]
-            # convert preferences to degrees and ensure it's within [0, 360)
+            # Get preferences in degrees and restrict range to [0, 360)
             preferences = np.arctan2(c2, c1) * (180/np.pi)
             preferences[preferences < 0] += 360
             preferences_rad = np.deg2rad(preferences)
-            # y = b0 + b1 * cos(x - preferences)
-            # multiply b1 by 2 in order to get the modulation
-            modulations = 2 * \
-                (c2 - c1)/(np.sin(preferences_rad) - np.cos(preferences_rad))
+            # Calculate modulations
+            modulations = 2 * (c2 - c1) / (np.sin(preferences_rad)
+                                           - np.cos(preferences_rad))
 
         elif form == 'cosine2':
-            # y = b0 + c1 cos(2x) + c2 sin(2x)
+            # Splt up cosine coefficients
             c1 = tuning_coefs[..., 0]
             c2 = tuning_coefs[..., 1]
-            # convert preferences to degrees and ensure it's within [0, 360)
+            # Get preferences in degrees and restrict range to [0, 360)
             preferences = np.arctan2(c2, c1) * (180/np.pi)
             preferences[preferences < 0] += 360
             preferences_rad = np.deg2rad(preferences)
-            # y = b0 + b1 * cos(2 (x - preferences))
-            modulations = 2 * \
-                (c2 - c1)/(np.sin(preferences_rad) - np.cos(preferences_rad))
-            # divide preferences by 2 and take the modulus to ensure preference
-            # lies within [0, 180)
+            # Ensure preference lies within [0, 180) due to the period
             preferences = (preferences / 2) % 180
+            # Calculate modulations
+            modulations = 2 * (c2 - c1) / (np.sin(preferences_rad)
+                                           - np.cos(preferences_rad))
 
         elif form == 'one_hot':
             preferences = 30 * np.argmax(tuning_coefs, axis=-1)
