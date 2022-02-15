e9c341b575e45a4b42c47d8363a6b87229f77134
Jesse Livezey <jesse.livezey@gmail.com>
2022-02-15 15:08:09 -0800

    starting base class

---
+++ neuropacks/nsds_nwb/nsds_nwb.py
@@ -0,0 +1,70 @@
+import scipy as sp
+
+from pynwb import NWBHDF5IO
+
+from types import SimpleNamespace
+
+
+class NSDS_NWB():
+    def __init__(self, nwb_path):
+        self.nwb_path = nwb_path
+        self.ecog = None
+        self.poly = None
+        self.stimulus = None
+        self._stimulus_envelope = None
+        self.mark = None
+        self.electrodes = None
+
+    def _load_ecog(self):
+        """Load ecog data, if available."""
+        ecog = []
+        with NWBHDF5IO(self.nwb_path, 'r') as io:
+            nwb = io.read()
+            di = nwb.processing['preprocessing'].data_interfaces
+            for n in di.keys():
+                if 'ecog' in n.lower():
+                    ecog.append(di[n].data[:])
+                    ecog_rate = di[n].rate
+        if len(ecog) == 1:
+            self.ecog = SimpleNamespace(data=ecog[0],
+                                        rate=ecog_rate)
+        elif len(ecog) == 0:
+            pass
+        else:
+            raise ValueError('Multiple ECoG sources found.')
+
+    def _load_poly(self):
+        """Load polytrode data, if available."""
+        poly = []
+        with NWBHDF5IO(self.nwb_path, 'r') as io:
+            nwb = io.read()
+            di = nwb.processing['preprocessing'].data_interfaces
+            for n in di.keys():
+                if 'poly' in n.lower():
+                    poly.append(di[n].data[:])
+                    poly_rate = di[n].rate
+        if len(poly) == 1:
+            self.poly = SimpleNamespace(data=poly[0],
+                                        rate=poly_rate)
+        elif len(poly) == 0:
+            pass
+        else:
+            raise ValueError('Multiple ECoG sources found.')
+
+    def _load_stimulus_waveform(self):
+        """Load the stimulus waveform."""
+        with NWBHDF5IO(self.nwb_path, 'r') as io:
+            nwb = io.read()
+            self.stimulus = SimpleNamespace(data=nwb.stimulus['stim_waveform'].data[:],
+                                            rate=nwb.stimulus['stim_waveform'].rate)
+
+    @property
+    def stimulus_envelope(self):
+        "The stimulus envelope computed through the Hilbert Transform."
+        if self._stimulus_envelope is None:
+            fftd = sp.fft.fft(self.stimulus.astype('float32'))
+            freq = sp.fft.fftfreq(self.stimulus.size)
+            fftd[freq <= 0] = 0
+            fftd[freq > 0] *= 2
+            self._stimulus_envelope = abs(sp.fft.ifft(fftd))
+        return self._stimulus_envelope
