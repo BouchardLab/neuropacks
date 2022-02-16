bb149b1123924fc8ec014aea0000721f94efc358
Jesse Livezey <jesse.livezey@gmail.com>
2022-02-16 14:27:12 -0800

    design and response methods

--- neuropacks/nsds_nwb/nsds_nwb.py
+++ neuropacks/nsds_nwb/nsds_nwb.py
@@ -1,8 +1,42 @@
+from types import SimpleNamespace
+import numpy as np
 import scipy as sp
 
 from pynwb import NWBHDF5IO
 
-from types import SimpleNamespace
+
+def intersect_intervals(*intervals):
+    intervals = np.concatenate([np.squeeze(np.array(i))[np.newaxis] for i in intervals], axes=0)
+    interval = np.array([intervals.max(axis=0), intervals.min(axis=0)])
+    if interval[0] >= interval[1]:
+        raise ValueError('No intersection in given intervals.')
+    return interval
+
+
+def normalize_neural(data, baseline, method='zscore'):
+    """Normalize neural data to baseline.
+
+    Parameters
+    ----------
+    data : ndarray
+        Data to normalize. Should have 1 additional axis at the 0th position compared to baseline.
+    baseline : ndarray
+        Baseline neural data.
+    method : str
+        How to normalize the neural data.
+        - 'zscore' computes (data - mean) / std
+        - 'ratio' computes data / mean
+    """
+    mean = baseline.mean(axis=0, keepdims=True)
+    std = baseline.std(axis=0, keepdims=True)
+    if method == 'zscore':
+        data -= mean[np.newaxis]
+        data /= std[np.newaxis]
+    elif method == 'ratio':
+        data /= mean[np.newaxis]
+    else:
+        raise ValueError
+    return data, mean, std
 
 
 class NSDSNWBAudio:
@@ -29,7 +63,7 @@ class NSDSNWBAudio:
         electrode_df : dataframe
             Electrode dataframe.
         intervals : dataframe
-            Intervals/trais tables.
+            Intervals/trails tables.
         """
         self.nwb_path = nwb_path
         self.ecog = None
@@ -56,13 +90,15 @@ class NSDSNWBAudio:
             for n in di.keys():
                 if 'ecog' in n.lower():
                     ecog.append(di[n].data[:])
-                    ecog_rate = di[n].rate
+                    rate = di[n].rate
+                    starting_time = di[n].starting_time
         if len(ecog) == 1:
             idxs = self.electrode_df['group_name'] == 'ECoG'
             good_electrodes = ~self.electrode_df['bad'].loc[idxs].values
             self.ecog = SimpleNamespace(data=ecog[0],
-                                        rate=ecog_rate,
-                                        good_electrodes=good_electrodes)
+                                        rate=rate,
+                                        good_electrodes=good_electrodes,
+                                        starting_time=starting_time)
         elif len(ecog) == 0:
             pass
         else:
@@ -77,13 +113,15 @@ class NSDSNWBAudio:
             for n in di.keys():
                 if 'poly' in n.lower():
                     poly.append(di[n].data[:])
-                    poly_rate = di[n].rate
+                    rate = di[n].rate
+                    starting_time = di[n].starting_time
         if len(poly) == 1:
             idxs = self.electrode_df['group_name'] == 'Poly'
             good_electrodes = ~self.electrode_df['bad'].loc[idxs].values
             self.poly = SimpleNamespace(data=poly[0],
-                                        rate=poly_rate,
-                                        good_electrodes=good_electrodes)
+                                        rate=rate,
+                                        good_electrodes=good_electrodes,
+                                        starting_time=starting_time)
         elif len(poly) == 0:
             pass
         else:
@@ -93,8 +131,10 @@ class NSDSNWBAudio:
         """Load the stimulus waveform."""
         with NWBHDF5IO(self.nwb_path, 'r') as io:
             nwb = io.read()
-            self.stimulus = SimpleNamespace(data=nwb.stimulus['stim_waveform'].data[:],
-                                            rate=nwb.stimulus['stim_waveform'].rate)
+            stim = nwb.stimulus['stim_waveform']
+            self.stimulus = SimpleNamespace(data=stim.data[:],
+                                            rate=stim.rate,
+                                            starting_time=stim.starting_time)
 
     @property
     def stimulus_envelope(self):
@@ -106,6 +146,3 @@ class NSDSNWBAudio:
             fftd[freq > 0] *= 2
             self._stimulus_envelope = abs(sp.fft.ifft(fftd))
         return self._stimulus_envelope
-
-    def baseline_data(self):
-        raise NotImplementedError
