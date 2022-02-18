54cc39bf6b2406e0b2165a0e7ef91eb75680af8a
Ji Hyun Bak <jihyun.bak@gmail.com>
2022-02-17 17:14:04 -0800

    simplify TIMIT design matrix - list of waveforms

--- neuropacks/nsds_nwb/timit.py
+++ neuropacks/nsds_nwb/timit.py
@@ -3,7 +3,6 @@ import numpy as np
 from process_nwb.resample import resample
 
 from neuropacks.nsds_nwb.nsds_nwb import NSDSNWBAudio
-from neuropacks.nsds_nwb.sound import make_spectrogram
 from neuropacks.nsds_nwb.utils import normalize_neural
 
 
@@ -11,74 +10,24 @@ class TIMIT(NSDSNWBAudio):
     def __init__(self, nwb_path):
         super().__init__(nwb_path)
 
-    def get_design_matrix(self, sample_rate=100., encoding='spectro', **kwargs):
+    def get_design_matrix(self):
         """Create the trial design matrix.
 
-        **** TENTATIVE FORMATS - work in progress ****
+        For TIMIT block, the design matrix simply returns the stimulus waveforms
+        (wrapped into a SimpleNamespace object) from each stimulus trial.
+        For timit998, for example, there should be 998 trials.
 
-        Parameters
-        ----------
-        sample_rate : float
-            Frequency in Hz to downsample the stimuli to.
-        encoding : str
-            How the design matrix should be encoded.
-            Options are ['wav', 'envelope', 'spectro'].
+        We could add more options for stimulus encoding (not just waveforms).
 
         Returns
         -------
         design : list
-            List where each item is for a stimulus trial.
-            Each item includes the stimulus design matrix and optionally other info.
+            List of SimpleNamespace objects. Each namespace item includes the
+            stimulus waveform data from a stimulus trial, along with other metadata.
         """
-        idxs = (self.intervals['sb'] == 's')
-        self.n_trials = len(idxs)   # stimulus trials only!
-
-        if 'wav' in encoding:
-            stim_list = self.get_trialized_stim(type='wav')
-            # design = stim_list
-            # downsample to sample_rate?? not very informative
-            design = []
-            for stim in stim_list:
-                resampled = resample(stim.data, sample_rate, stim.rate,
-                                     real=True, axis=0)
-                design.append(resampled)
-
-        elif 'env' in encoding:
-            design = self.get_trialized_stim(type='env')
-            # raise NotImplementedError('TODO')
-
-        elif 'spectro' in encoding:
-            stim_list = self.get_trialized_stim(type='wav')
-            design = []
-            for stim in stim_list:
-                t, f, spectro = make_spectrogram(stim.data, stim.rate,
-                                                 spec_sample_rate=sample_rate,
-                                                 **kwargs)
-                t_shifted = t + stim.starting_time
-                design.append((t_shifted, f, spectro))
-        else:
-            raise ValueError(f"`encoding` must be one of ['wav', 'spectro']; was {encoding}")
+        design = self.get_trialized_stim(type='waveform')
+        self.n_trials = len(design)   # stimulus trials only!
         return design
-
-    def _get_trialized_spectrograms(self):
-        stim_ns = self._get_stimulus_waveform()
-
-        stim_wav_list = []
-        for ii, row in self.intervals.iterrows():
-            if row['sb'] != 's':
-                continue
-
-            if in_memory:
-                idx = slice_interval(row['start_time'], row['stop_time'],
-                                     rate=stim_ns.rate,
-                                     t_offset=stim_ns.starting_time)
-                stim_sliced = stim_ns.data[idx]
-            else:
-                stim_sliced = self._get_stimulus_waveform(
-                    start_time=row['start_time'],
-                    stop_time=row['stop_time']).data
-            stim_wav_list.append(stim_sliced)
-        return stim_wav_list
 
     def get_response_matrix(self, neural_data='ecog', in_memory=True):
         """Create the neural response matrix.
