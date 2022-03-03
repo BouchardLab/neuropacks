ef2cdfe49c3e0dfcbf35f8efcadfd89912907440
Ji Hyun Bak <jihyun.bak@gmail.com>
2022-03-03 10:12:10 -0800

    add function get_nsds_nwb_neuropack

--- neuropacks/nsds_nwb/__init__.py
+++ neuropacks/nsds_nwb/__init__.py
@@ -1,3 +1,17 @@
 from .nsds_nwb import NSDSNWBAudio
 from .continuous import TIMIT, DynamicMovingRipples
 from .discrete import Tone, WhiteNoise
+
+
+def get_nsds_nwb_neuropack(nwb_path, stim_name):
+    if stim_name == 'dmr':
+        return DynamicMovingRipples(nwb_path)
+    if stim_name == 'timit':
+        return TIMIT(nwb_path)
+    if 'tone' in stim_name:
+        return Tone(nwb_path)
+    if 'wn' in stim_name:
+        return WhiteNoise(nwb_path)
+
+    raise ValueError(f'Unknown stimulus name {stim_name}.'
+                     'Recognized names are [dmr, timit, tone*, wn*].')
