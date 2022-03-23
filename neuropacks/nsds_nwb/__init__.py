4fbdc1d75c2ef3bdf27b59317c2551711f064b0b
Ji Hyun Bak <jihyun.bak@gmail.com>
2022-03-23 12:36:44 -0700

    add stim auto-detection in get_nsds_nwb_neuropack

--- neuropacks/nsds_nwb/__init__.py
+++ neuropacks/nsds_nwb/__init__.py
@@ -1,9 +1,20 @@
-from .nsds_nwb import NSDSNWBAudio
+from pynwb import NWBHDF5IO
+
+from .nsds_nwb import NSDSNWBAudio, detect_stim_for_session
 from .continuous import TIMIT, DynamicMovingRipples
 from .discrete import Tone, WhiteNoise
 
 
-def get_nsds_nwb_neuropack(nwb_path, stim_name):
+def get_nsds_nwb_neuropack(nwb_path, stim_name=None):
+    if stim_name is None:
+        with NWBHDF5IO(nwb_path, 'r') as io:
+            nwb = io.read()
+            stim_name, _ = detect_stim_for_session(nwb.session_description)
+
+    if stim_name is None:
+        raise ValueError('Stimulus name could not be automatically detected. '
+                         'Please provide a stim_name.')
+
     if stim_name == 'dmr':
         return DynamicMovingRipples(nwb_path)
     if stim_name == 'timit':
